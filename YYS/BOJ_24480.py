3f6ec17da6e5f71ad65967cc4734eebfdf62c4f9
devyoseph <josephdev123@naver.com>
2022-06-20 09:59:10 +0900

    [BOJ] 알고리즘 수업 - 깊이 우선 탐색 2 / 실버2 / 5분

---
+++ YYS/BOJ_24480.py
@@ -0,0 +1,32 @@
+import sys
+sys.setrecursionlimit(100000)
+input = sys.stdin.readline
+
+def dfs(arr, visit, res, n):
+    global number
+    visit[n] = True
+    res[n] = number
+    number = number + 1
+    for x in arr[n]:
+        if not visit[x]:
+            dfs(arr, visit, res, x)
+
+N, M, R = map(int, input().split())
+# arr = [[0 for _ in range(N)] for __ in range(N)]
+arr = [list() for _ in range(N)]
+res = [0]*N # 결과 저장
+visit = [False]*N
+number = 1
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    arr[a-1].append(b-1)
+    arr[b-1].append(a-1)
+
+for a in arr:
+    a.sort(reverse=True);
+
+dfs(arr, visit, res, R-1)
+
+for i in range(N):
+    print(res[i])
