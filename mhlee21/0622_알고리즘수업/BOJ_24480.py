172126d01efcfe524a9990df68ab59993a44b195
mhlee <mihyun0921@gmail.com>
2022-06-21 14:03:03 +0900

    [BOJ] 알고리즘 수업 - 깊이 우선 탐색 2 / 실버 2 / 10분
    https://www.acmicpc.net/problem/24480

---
+++ mhlee21/0622_알고리즘수업/BOJ_24480.py
@@ -0,0 +1,36 @@
+"""
+BOJ
+알고리즘 수업 - 깊이 우선 탐색 2
+실버 2
+10분
+https://www.acmicpc.net/problem/24480
+"""
+#pypy3 제출
+
+import sys
+sys.setrecursionlimit(100000)
+
+N, M, R = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+visited = [0]*(N+1)
+
+def dfs(node):
+    global cnt
+    visited[node] = cnt
+    cnt += 1
+    for next in graph[node]:
+        if visited[next] == 0:
+            dfs(next)
+
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for i in range(1,N+1):
+    graph[i].sort(reverse=True)
+
+cnt = 1
+dfs(R)
+for i in visited[1:]:
+    print(i)
